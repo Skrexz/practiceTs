1a0aa7280c56bef6da31ef1dadd8ba2c45e2187c
Skrexz <xsebastianrojasx@gmail.com>
2023-07-28 14:08:00 -0500

    Se agrego funciones

--- 01-reforzamiento/src/App.tsx
+++ 01-reforzamiento/src/App.tsx
@@ -1,11 +1,15 @@
-import { TiposBasicos } from './typescript/TiposBasicos';
+//import { TiposBasicos } from './typescript/TiposBasicos';
+import { Funciones } from './typescript/Funciones';
+//import { ObjetosLiterales } from './typescript/ObjetosLiterales';
 
 const App = () => {
   return(
     <div className="mt-2">
       <h1>Introduccion a TS -React </h1>
-      <hr />
-      <TiposBasicos />
+      <hr/>
+      {/*<TiposBasicos/> */}
+      {/*<ObjetosLiterales/>*/}
+      <Funciones/>
     </div>
   )
 }
